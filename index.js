70d318e3d956881ca6db5df628ecca9fd28aa4e6
sandberg-smith <sandbergjacques500@gmail.com>
2021-06-21 15:53:50 +0200

    add index.js

---
+++ index.js
@@ -0,0 +1,58 @@
+var fs = require("fs");
+const { exec } = require("child_process");
+
+var path = require("path");
+const { FILE } = require("dns");
+var walk = function (dir, done) {
+  var results = [];
+  fs.readdir(dir, function (err, list) {
+    if (err) return done(err);
+    var i = 0;
+    (function next() {
+      var file = list[i++];
+      if (!file) return done(null, results);
+      let file1 = file;
+      file = path.resolve(dir, file);
+      fs.stat(file, async function (err, stat) {
+        if (stat && stat.isDirectory()) {
+          walk(file, function (err, res) {
+            next();
+          });
+        } else {
+          // if (!file.contains(".git")) {
+          //   await exec('git add "' + file + '"', (error, stdout, stderr) => {
+          //     if (error) {
+          //       console.log(`error: ${error.message}`);
+          //       return;
+          //     }
+          //     if (stderr) {
+          //       console.log(`stderr: ${stderr}`);
+          //       return;
+          //     }
+          //     console.log(`stdout: ${stdout}`);
+          //   });
+
+          //   await exec("git commit -m " + file, (error, stdout, stderr) => {
+          //     if (error) {
+          //       console.log(`error: ${error.message}`);
+          //       return;
+          //     }
+          //     if (stderr) {
+          //       console.log(`stderr: ${stderr}`);
+          //       return;
+          //     }
+          //     console.log(`stdout: ${stdout}`);
+          //   });
+          // }
+          results.push(file);
+          next();
+        }
+      });
+    })();
+  });
+};
+
+walk(".", function (err, results) {
+  if (err) throw err;
+  console.log(results);
+});
